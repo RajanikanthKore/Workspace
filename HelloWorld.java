8b19e35897acad92abac6f622c8eced5a5a822b3
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-04-02 00:20:03 +0530

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+package com.alian.fullstack;
+
+
+class Myname
+{
+	String a="Rajanikanth";
+	String b="Kore";
+	
+	String add()
+	{
+		String c=a + b;
+		return c;
+	}
+	
+}
+
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		 Myname myname= new Myname();
+		 
+		String result=myname.add();
+		 System.out.println(result);
+
+	}
+
+}
